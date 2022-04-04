f9ed88bca0103ad790f11497f92f7a49c60f6419
AlirezaDanaee <ywarior24@gmail.com>
2022-04-04 14:12:16 +0430

    remove picture

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path , include , re_path
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 from account.views import Register ,activate
 
@@ -38,4 +38,4 @@ urlpatterns = [
 
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
